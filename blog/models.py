70872edd63f15ac6494a7242e49e6f444e34ed6a
NimaHeydarzadeh <nima.nami.hdrz@gmail.com>
2022-05-25 20:07:05 +0430

    blog/models updated

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse,reverse_lazy
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    bio = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.user.username
+    
+    def get_absolute_url(self):
+        return reverse_lazy("profile", kwargs={"username": self.user.username})
+
+        
+
+class Comment(models.Model):
+    pass
+
+
+class Post(models.Model):
+    pass
