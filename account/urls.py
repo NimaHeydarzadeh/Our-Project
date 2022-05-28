ca0545fd8c8d3fc47ecc4a525ecc74ff4a96b413
NimaHeydarzadeh <nima.nami.hdrz@gmail.com>
2022-05-28 21:11:49 +0430

    add url to href 'about.html'

--- account/urls.py
+++ account/urls.py
@@ -5,5 +5,4 @@ from account import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
 ]
--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from blog import views
 
 
-
 urlpatterns = [
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('developer/', views.contact, name='developer'),
     path('home/', views.home, name='home'),
     path('post/', views.post, name='post'),
     path('posts/', views.posts, name='posts'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
 ]
