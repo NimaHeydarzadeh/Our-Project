7498d7d1304ca39f9909d2969ef3c326eea2053c
NimaHeydarzadeh <nima.nami.hdrz@gmail.com>
2022-05-29 22:52:35 +0430

    update admin.py, blog/modles.py

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,8 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # picture = models.ImageField(upload_to='profile_images', blank=True)
     bio = models.CharField(max_length=200, blank=True, null=True)
+    create_date = models.DateTimeField(auto_now_add=True,null = True)
+    modify_date = models.DateTimeField(auto_now=True,null = True)
 
     def __str__(self):
         return self.user.username
@@ -19,13 +21,16 @@ class UserProfile(models.Model):
 
 
 class Post(models.Model):
+   
     slug = models.SlugField(max_length=100, blank=True)
     auth = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    create_date = models.DateTimeField(auto_now_add=True,null = True)
+    modify_date = models.DateTimeField(auto_now=True,null = True)
     title = models.CharField(max_length=200)
     body = models.TextField()
     date = models.DateField(auto_now_add=True)
     like = models.ManyToManyField(UserProfile, related_name="post_like")
-    dislike = models.ManyToManyField(UserProfile, related_name="post_dislike")
+    dislike = models.ManyToManyField(UserProfile, related_name="post_dislike",blank=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
