37824e9cbbbd403cb3475192b166fdd226bbe7a8
NimaHeydarzadeh <nima.nami.hdrz@gmail.com>
2022-05-30 23:10:43 +0430

    abcd

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelatio
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # picture = models.ImageField(upload_to='profile_images', blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
     bio = models.CharField(max_length=200, blank=True, null=True)
     create_date = models.DateTimeField(auto_now_add=True, null=True)
     modify_date = models.DateTimeField(auto_now=True, null=True)
