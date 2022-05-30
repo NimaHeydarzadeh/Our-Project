35390c77e1cee0bea785ad70f69e336a5b7baf00
FE27 <FE27@users.noreply.github.com>
2022-05-30 15:59:38 +0330

    some bugs fixed

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from blog import views
 
-
 urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('developer/', views.contact, name='developer'),
+    path('developer/', views.developer, name='developer'),
     path('home/', views.home, name='home'),
     path('index/', views.home, name='index'),
     path('post/', views.post, name='post'),
     path('posts/', views.posts, name='posts'),
 ]
+
